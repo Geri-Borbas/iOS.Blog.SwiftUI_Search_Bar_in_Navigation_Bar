ac0c2f9fd9b2edf69bb044bc5c0f5c2cc02e72df
Geri Borbás <geri@eppz.eu>
2020-04-27 01:24:33 +0200

    Initial Commit

---
+++ SwiftUI_Search_Bar_in_Navigation_Bar/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUI_Search_Bar_in_Navigation_Bar
+//
+//  Created by Geri Borbás on 2020. 04. 27..
+//  Copyright © 2020. Geri Borbás. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
