6aa69e8a7c24deced5b2dd7ada579864d3be4ac6
Geri Borbás <geri@eppz.eu>
2020-04-27 17:50:07 +0200

    0.0.4

---
+++ SwiftUI_Search_Bar_in_Navigation_Bar/SearchControllerProvider.swift
@@ -0,0 +1,21 @@
+//
+//  SearchController.swift
+//  SwiftUI_Search_Bar_in_Navigation_Bar
+//
+//  Created by Geri Borbás on 2020. 04. 27..
+//  Copyright © 2020. Geri Borbás. All rights reserved.
+//
+
+import UIKit
+
+
+class SearchControllerProvider
+{
+    
+    
+    let searchController = UISearchController(searchResultsController: nil)
+    
+    
+    init
+    
+}
